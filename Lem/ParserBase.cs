6d5bd72b1d63dc74c89f95687fac102947229702
Владислав Крылов <krylov@sfedu.ru>
2023-07-25 14:05:52 +0300

    Create ParserBase.cs

---
+++ Lem/ParserBase.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lem
+{
+    internal class ParserBase
+    {
+        private Lexer lex;
+        protected int current;
+        protected Token curToken;
+        public ParserBase(Lexer lex)
+        {
+            this.lex = lex;
+
+        }
+        public Token NextLexem()
+        {
+            var res = curToken;
+            curToken = lex.NextToken();
+            if (!IsAtEnd()) current++;
+            return res;
+        }
+        public bool At(params TokenType[] types) => types.Any(type => PeekToken().type == type);
+        public void Check(params TokenType[] types)
+        {
+            if (!At(types)) throw new Exception();
+        }
+        public bool IsMatch(params TokenType[] types)
+        {
+            var res = At(types);
+            if (res) NextLexem();
+            return res;
+        }
+        public Token Requires(params TokenType[] types)
+        {
+            if (At(types))
+                return NextLexem();
+            else throw new Exception();
+        }
+        public bool IsAtEnd() => PeekToken().type == TokenType.Eof;
+        public Token PeekToken() => curToken;
+    }
+}
